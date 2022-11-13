26c1c08bbf9b6f4d04c3773d9c8e23d4eb2a22b5
= <=>
2022-11-12 22:16:35 -0300

    Updates

---
+++ precache-manifest.2fd870f82f3cc8e408cf8af9efc13e8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "197140220e72b514eaa7",
+    "url": "/jesters-website/build/static/css/main.6f333d53.chunk.css"
+  },
+  {
+    "revision": "197140220e72b514eaa7",
+    "url": "/jesters-website/build/static/js/main.19714022.chunk.js"
+  },
+  {
+    "revision": "295a49fdf4c168de2357",
+    "url": "/jesters-website/build/static/js/1.295a49fd.chunk.js"
+  },
+  {
+    "revision": "53b19bc00ab88543cdc3",
+    "url": "/jesters-website/build/static/css/2.0d70e960.chunk.css"
+  },
+  {
+    "revision": "53b19bc00ab88543cdc3",
+    "url": "/jesters-website/build/static/js/2.53b19bc0.chunk.js"
+  },
+  {
+    "revision": "98aaa269eaa3d9973bd5",
+    "url": "/jesters-website/build/static/js/runtime~main.98aaa269.js"
+  },
+  {
+    "revision": "b846e42b7caeadf1854f8739f4966e36",
+    "url": "/jesters-website/build/static/media/background-video.b846e42b.webm"
+  },
+  {
+    "revision": "1566722df2c45bc654c68a5e33c8e2b4",
+    "url": "/jesters-website/build/index.html"
+  }
+];
